51b2ec699cde7b3bc775fa568707faf8e0104de7
Masato-88 <shigashi32@gmail.com>
2023-06-30 01:23:26 -0700

    Seed route connected

---
+++ models/seed.js
@@ -0,0 +1,68 @@
+const songs = [
+    {
+        name: "Perspective",
+        artist: "Rokazer",
+        album: "Perspective EP",
+        photo: "https://geo-media.beatport.com/image_size/500x500/9eb36c9a-928e-47d4-8695-3022f3316204.jpg",
+        timeLength: { type: String, required: true },
+    },
+    
+    {
+        name: "Bye Bye Macadam",
+        artist: "Rone",
+        album: "Tohu Bohu",
+        photo: "/https://i.scdn.co/image/ab67616d0000b2730676758cf1f521b60f178274",
+        timeLength: "4:25",
+    },
+
+    {
+        name: "Tenderness - Woven Remix",
+        artist: "Tony Anderson, Christopher Dennis Coleman, Woven",
+        album: "Nuit",
+        photo: "https://f4.bcbits.com/img/a3378407599_10.jpg",
+        timeLength: "3:50",
+    },
+
+    {
+        name: "Dave Saints",
+        artist: "Tom Demac",
+        album: "The Shuttle Awaits",
+        photo: "https://f4.bcbits.com/img/a0160841134_65",
+        timeLength: "7:06",
+    },
+
+    {
+        name: "Arcadia - Telefon Tel Aviv Remix",
+        artist: "Apparat, Telefon Tel Aviv",
+        album: "Apparat/Arcadia RMXS",
+        photo: "https://f4.bcbits.com/img/a1666307554_65",
+        timeLength: "7:48",
+    },
+
+    {
+        name: "Crystal Forest",
+        artist: "Kevin Di Serna, Ditian",
+        album: "Single Crystal Forest/Dallah",
+        photo: "/https://is5-ssl.mzstatic.com/image/thumb/Music18/v4/c0/be/fa/c0befa47-3f57-90f0-b755-3c4da5ff4e4d/5414165079965.png/1200x1200bf-60.jpg",
+        timeLength: "6:48",
+    },
+
+    {
+        name: "Dancing Sun",
+        artist: "Petar Dundov",
+        album: "Single Dancing Sun",
+        photo: "https://i.discogs.com/IXoXotJQqhDdcZCEn23M65DiHAZXnHKIlMXMrYizlCM/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTg0OTE4/NTQtMTQ2MjY3NjI1/Ni03ODU2LmpwZWc.jpeg",
+        timeLength: "8:08",
+    },
+    
+    {
+        name: "Tempest",
+        artist: "Pional",
+        album: "Single Miracle/Tempest",
+        photo: "https://f4.bcbits.com/img/a1545213290_10.jpg",
+        timeLength: "5:25",
+    }
+]
+
+// Export the seed data to 'models/index.js'
+module.exports = songs
