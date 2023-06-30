e81e670208b271f3b255b73584262b793f20ff99
Masato-88 <shigashi32@gmail.com>
2023-06-30 16:18:22 -0700

    Index, Show, New, and create route working

--- controllers/songs.js
+++ controllers/songs.js
@@ -14,7 +14,18 @@ const db = require('../models')
 // Index Route (GET/Read): Will display all songs
 router.get('/', function (req, res) {
     db.Song.find({})
-        .then(songs => res.json(songs))
+        .then(songs => {
+            res.render('song-index', {
+                songs: songs
+            })
+        })
+})
+
+
+// New Route (GET/Read): This route renders a form 
+// which the user will fill out to POST (create) a new location
+router.get('/new', (req, res) => {
+    res.send('You\'ve hit the new route!')
 })
 
 
@@ -22,9 +33,22 @@ router.get('/', function (req, res) {
 // using the URL parameter (which is the document _id)
 router.get('/:id', function (req, res) {
     db.Song.findById(req.params.id)
-        .then(song => res.json(song))
+        .then(song => {
+            res.render('song-details', {
+                song: song
+            })
+        })
         .catch(() => res.send('404 Error: Page Not Found'))
 })
+
+// Create Route (POST/Create): This route receives the POST request sent from the new route,
+// creates a new pet document using the form data, 
+// and redirects the user to the new pet's show page
+router.post('/', (req, res) => {
+    db.Song.create(req.body)
+        .then(song => res.json(song))
+})
+
 
 
 /* Export these routes so that they are accessible in `server.js`
