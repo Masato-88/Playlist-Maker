181725e2ec4a6971206720928011e34a97a00a96
Masato-88 <shigashi32@gmail.com>
2023-07-10 06:04:34 -0700

    styling revamped

--- models/song.js
+++ models/song.js
@@ -2,12 +2,13 @@
 const mongoose = require('mongoose');
 const playlistSchema = require('./playlist.js')
 
-// Create a schema to define the properties of the pets collection
+// Create a schema to define the properties of the songs collection
 const songSchema = new mongoose.Schema({
     name: { type: String, required: true },
     artist: { type: String, required: true },
     photo: { type: String, required: true },
-    timeLength: {type: String, required: true },
+    album: { type: String, required: true },
+    timeLength: {type: String, required: false },
     // the applications array can only accept objects that match the criteria specified
     // in the applicationSchema. In other words, the applications array can only accept applications
 	playlists: [playlistSchema]
